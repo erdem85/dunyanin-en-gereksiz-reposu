4f07ea3a19b7534e8e6fce34f15c3f8891e7590e
erdem <erdemhso@mail.ru>
2020-09-26 20:52:37 +0300

    Add files via upload

---
+++ low.js
@@ -0,0 +1,25 @@
+const za = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync('db.json')
+const low = za(adapter)
+
+module.exports = {
+	set: function(x, y){
+		low.set(x, y).write()
+	},
+	get: function(x){
+		return low.get(x).value()
+	},
+	push: function(x, y){
+		low.get(x).push(y).write()
+	},
+	has: function(x){
+		return low.has(x).value()
+	}
+}
+
+// set: (x, y) => db.set(x, y).write()
+// get: x => db.get(x).value()
+// push: (x, y) => db.get(x).push(y).write()
+// has: x => db.has(x).value()
